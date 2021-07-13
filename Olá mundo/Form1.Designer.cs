059eda017839fc7667973bb05e39738c1a6cc5bc
Denis78-sys <denismarques736@gmail.com>
2021-07-13 08:55:59 -0300

    Acrescentado um botao OK

--- Olá mundo/Form1.Designer.cs
+++ Olá mundo/Form1.Designer.cs
@@ -29,6 +29,7 @@
         private void InitializeComponent()
         {
             this.label1 = new System.Windows.Forms.Label();
+            this.Ok = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label1
@@ -40,11 +41,21 @@
             this.label1.TabIndex = 0;
             this.label1.Text = "Olá mundo";
             // 
+            // Ok
+            // 
+            this.Ok.Location = new System.Drawing.Point(579, 239);
+            this.Ok.Name = "Ok";
+            this.Ok.Size = new System.Drawing.Size(75, 23);
+            this.Ok.TabIndex = 1;
+            this.Ok.Text = "button1";
+            this.Ok.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Ok);
             this.Controls.Add(this.label1);
             this.Name = "Form1";
             this.Text = "Form1";
@@ -56,6 +67,7 @@
         #endregion
 
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button Ok;
     }
 }
 
